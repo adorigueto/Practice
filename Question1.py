3d580025d7f97bbac2c1936b984ce762b6c3abf9
André Canal <93547260+adorigueto@users.noreply.github.com>
2022-08-11 13:17:34 -0300

    Question 1 solved

---
+++ Question1.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+
+def login_table (id_name_verified, id_password):
+    id_name_verified["Password"] = pd.DataFrame(id_password)[1]
+    id_name_verified.drop("Verified", axis=1, inplace=True)
+
+
+id_name_verified = pd.DataFrame([[1, "JohnDoe", True], [2, "AnnFranklin", False]], columns=["Id", "Login", "Verified"])
+id_password = np.array([[1, 987340123], [2, 187031122]], np.int32)
+login_table(id_name_verified, id_password)
+print(id_name_verified)
